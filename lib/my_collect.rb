0edc0b761950fc3313897dccfc4bfcda1f2cb0cd
Elle Valentine <elle.valentine@hotmail.com>
2019-04-02 10:08:37 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,30 @@
+def my_collect(collection)
+    i = 0
+    while i <  collection.length
+      yield(collection[i])
+      i = i + 1
+    end
+ return collection.map
+end
+
+
+
+
+languages = ['ruby', 'javascript', 'python', 'objective-c']
+
+my_collect(languages) do |y|
+  y.upcase
+end
+
+
+students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+my_collect(students) do |x|
+  x.split(" ").first
+end
+
+
+
+
 
 
